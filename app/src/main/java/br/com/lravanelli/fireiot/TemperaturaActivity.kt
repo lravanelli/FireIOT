7e528621b96b52d8d1c20ecc96e4e16bf7ae9252
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-03 22:06:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lravanelli/fireiot/TemperaturaActivity.kt
@@ -0,0 +1,33 @@
+package br.com.lravanelli.fireiot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_temperatura.*
+
+class TemperaturaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_temperatura)
+
+        lerTemperatura()
+    }
+
+    private fun lerTemperatura() {
+        val database = FirebaseDatabase.getInstance().reference
+        database.child("temperatura").addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                val temp = p0?.getValue(Double::class.java)
+                tvTemperatura.text = temp.toString()
+            }
+        })
+    }
+}
